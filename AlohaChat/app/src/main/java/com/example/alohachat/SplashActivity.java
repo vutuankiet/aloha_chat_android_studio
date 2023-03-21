2a8f85c99d912b29226f5adfe6a3d0f78a825276
vutuankiet <vutuankiet2411@gmail.com>
2023-03-21 20:00:17 +0700

    First code Signed-off-by: vutuankiet <vutuankiet2411@gmail.com>

---
+++ AlohaChat/app/src/main/java/com/example/alohachat/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.alohachat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private class DownloadDataTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            // Tải dữ liệu ở đây
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            // Sau khi tải dữ liệu xong, chuyển đến MainActivity
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new DownloadDataTask().execute();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 10000);
+    }
+}
